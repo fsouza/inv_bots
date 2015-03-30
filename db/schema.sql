2c77a7664d19884bf785b521825585a759b79c33
Francisco Souza <f@souza.cc>
2015-03-29 21:51:47 -0300

    db: removing package
    See c45088358972d6e525c1246e84c0c4dd89af35b8.

--- db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE cvm_material_facts (
-	fact_id SERIAL PRIMARY KEY,
-	send_date VARCHAR(55) NOT NULL,
-	reference_date VARCHAR(55) NOT NULL,
-	company VARCHAR(255) NOT NULL,
-	subject VARCHAR(255) NOT NULL,
-	protocol_number VARCHAR(255) NOT NULL
-);
